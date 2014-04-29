3a612e728c6b5aed29d9e05cd860861b49f62692
Chris <ichrs@users.noreply.github.com>
2014-04-28 22:43:22 -1000

    Removed meta tags

--- header.php
+++ header.php
@@ -6,15 +6,7 @@
 <title><?php bloginfo('name'); ?><?php wp_title(" : ",true); ?></title>
 <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 <meta name="dc.language" content="en" />
-<meta name="author" content="chris w" />
-<meta name="robots" content="noodp" />
-<meta name="robots" content="noydir" />
-<meta name="verify-v1" content="nBgueJMzoOudE1CvE/iuVsp/02vtW9Xroknb+oFntYs=" />
 <meta name="description" content="<?php bloginfo('description'); ?>" />
-<meta name="robots" content="index,nofollow" />
-<meta name="googlebot" content="noarchive" />
-<meta name="robots" content="noimageindex" />
-<meta name="robots" content="nocache,noarchive" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen, print" />
 <link href='http://fonts.googleapis.com/css?family=Lustria' rel='stylesheet' type='text/css'>
